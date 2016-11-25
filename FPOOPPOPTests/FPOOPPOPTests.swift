a97a460ce71f816d6c0aecdc7bd39dd5a1f6d5ed
檜枝　龍一 <r_hieda@waku-2.com>
2016-11-25 18:07:32 +0900

    Initial Commit

---
+++ FPOOPPOPTests/FPOOPPOPTests.swift
@@ -0,0 +1,36 @@
+//
+//  FPOOPPOPTests.swift
+//  FPOOPPOPTests
+//
+//  Created by 檜枝　龍一 on 11/25/16.
+//  Copyright © 2016 檜枝　龍一. All rights reserved.
+//
+
+import XCTest
+@testable import FPOOPPOP
+
+class FPOOPPOPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
